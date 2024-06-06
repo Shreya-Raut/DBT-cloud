ca23dbbf1136f319bf6b582c0397d2f7e3ead9dc
Shreya Raut <shreya.raut@thetrainline.com>
2024-06-06 07:43:41 +0000

    commit

--- dbt-login/dtb_trino_test/macros/customer.sql
+++ dbt-login/dtb_trino_test/macros/customer.sql
@@ -13,21 +13,6 @@
             then 'United Kingdom'
             else region
         end as region,
-
-<<<<<<< HEAD
-        'active_customers' as metric_name,
-        'Active Customers' as metric_label,
-        sum(customers) as metric_value
-    from {{ source("tableau", "customer_growth_accounting") }} cga
-    join {{ source("calender", "bi_d_calendar") }} cal on cga."week" = cal.report_date
-    where
-        metric_label = 'Customer Base'
-        and split(managed_group_id, ' ')[1] in ('20', '42')
-        and person_status in ('New', 'Resurrected', 'Retained')
-        {% if is_incremental() %}
-            AND DATE(cal.report_week_commencing) = DATE('2024-05-26')
-        {% endif %}  -- active_customers
-=======
     'active_customers' AS metric_name,
     'Active Customers' AS metric_label,
     SUM(customers) AS metric_value
